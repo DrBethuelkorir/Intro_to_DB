0bc3dc2758f18a346ac83e81d0bf7f3b3f31e0d8
Dr.sheriff <kiptoobethuel44@gmail.com>
2025-10-16 17:14:37 +0300

    DONE

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,14 @@
-SELECT * FROM books;
+SELECT 
+    COLUMN_NAME,
+    DATA_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    information_schema.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books'
+ORDER BY 
+    ORDINAL_POSITION;
