7c6925c4b7a850d60702796c7a2da8ffe31160c9
Dr.sheriff <kiptoobethuel44@gmail.com>
2025-10-16 16:53:59 +0300

    task_2.sql

---
+++ task_2.sql
@@ -0,0 +1,33 @@
+
+CREATE table Authors(
+    author_id INT PRIMARY key,
+    author_name VARCHAR(215)
+
+);
+
+CREATE TABLE Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    pulication_date DATE
+);
+
+CREATE Table Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+CREATE Table Orders(
+    order_id int PRIMARY key,
+    customer_id int,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+CREATE TABLE Order_Details(
+    orderdetailid int PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE
+);
